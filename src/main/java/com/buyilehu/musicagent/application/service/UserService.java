c645a748472f797c459c0663909399ff330d45e5
Serendipity <112629775+haolu111@users.noreply.github.com>
2026-06-27 15:32:38 +0800

    Delete src directory

--- src/main/java/com/buyilehu/musicagent/application/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.buyilehu.musicagent.application.service;
-
-import com.buyilehu.musicagent.application.dto.response.UserResponse;
-
-public interface UserService {
-    UserResponse getById(Long id);
-}
